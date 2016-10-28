b2c968c57b5a7cd65d32a3e8270638a48a3ff6f8
David Eldon <deldon@princeton.edu>
2016-10-27 21:12:40 -0700

    wip some work on euler03

---
+++ euler03.m
@@ -0,0 +1,13 @@
+%Project Euler #3
+%
+%The prime factors of 13195 are 5, 7, 13 and 29.
+%
+%What is the largest prime factor of the number 600851475143 ?
+
+target=600851475143;
+
+%step one: find primes with a prime seive
+
+x=1:target;
+
+x(end)
