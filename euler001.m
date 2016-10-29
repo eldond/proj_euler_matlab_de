8d95971ab8f8ce604d08a5682a15412f02f2a671
David Eldon <deldon@princeton.edu>
2016-10-28 20:35:22 -0700

    Changed solution file header format

--- euler001.m
+++ euler001.m
@@ -1,7 +1,12 @@
-%Project Euler problem 1: multiples of 3 and 5
-%If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+% Project Euler Problem 1: Multiples of 3 and 5
 %
-%Find the sum of all the multiples of 3 or 5 below 1000.
+% projecteuler.net
+%
+% If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+%
+% Find the sum of all the multiples of 3 or 5 below 1000.
+%
+% Solution by David Eldon 2016-08-28
 
 a=3; %the first number we're interested in
 b=5; %the second number we're interested in
--- euler003.m
+++ euler003.m
@@ -1,8 +1,12 @@
-%Project Euler #3
+% Project Euler Problem 3: Largest prime factor
 %
-%The prime factors of 13195 are 5, 7, 13 and 29.
+% projecteuler.net
 %
-%What is the largest prime factor of the number 600851475143 ?
+% The prime factors of 13195 are 5, 7, 13 and 29.
+%
+% What is the largest prime factor of the number 600851475143 ?
+%
+% Solution by David Eldon 2016-10-27
 
 target=600851475143;
 
